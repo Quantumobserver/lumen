35ac27e6816e0fbc8e4ab95e16daecb95f003797
Quantumobserver <yufanchen1244@gmail.com>
2024-12-25 23:50:13 -0700

    Implemented the ReadWritePtr

---
+++ src/Core/Memory/ReadWritePtr.hpp
@@ -0,0 +1,73 @@
+
+#pragma once
+
+#include <cassert>
+
+namespace Lumen {
+namespace Core {
+namespace Memory {
+
+template<typename T>
+class ReadWritePtr {
+private:
+        T *m_pointer;
+public:
+
+        constexpr ReadWritePtr(void) noexcept : m_pointer{nullptr} {}
+        constexpr ReadWritePtr(std::nullptr_t) noexcept : m_pointer{nullptr} {}
+        constexpr ReadWritePtr(T *pointer) noexcept : m_pointer{pointer} {}
+
+        constexpr ReadWritePtr(const ReadWritePtr &other) noexcept = default;
+        constexpr ReadWritePtr &operator=(const ReadWritePtr &other) noexcept = default;
+
+        constexpr ReadWritePtr(ReadWritePtr &&other) noexcept : m_pointer{other.m_pointer}
+        {
+                other.m_pointer = nullptr;
+        }
+
+        constexpr ReadWritePtr &operator=(ReadWritePtr &&other) noexcept
+        {
+                if (this == &other) {
+                        return *this;
+                }
+
+                this->m_pointer = other.m_pointer;
+                other.m_pointer = nullptr;
+                return *this;
+        }
+
+        constexpr ReadWritePtr &operator=(std::nullptr_t) noexcept
+        {
+                this->m_pointer = nullptr;
+                return *this;
+        }
+
+        constexpr ReadWritePtr &operator=(T *pointer) noexcept
+        {
+                this->m_pointer = pointer;
+                return *this;
+        }
+
+        constexpr T *Get(void) const noexcept
+        {
+                return m_pointer;
+        }
+
+        constexpr operator bool(void) const noexcept
+        {
+                return nullptr != m_pointer;
+        };
+
+        constexpr T &operator*(void) const noexcept
+        {
+                return *m_pointer;
+        }
+        constexpr T *operator->(void) const noexcept
+        {
+                return m_pointer;
+        }
+};
+
+} // namespace Memory
+} // namespace Core
+} // namespace Lumen
